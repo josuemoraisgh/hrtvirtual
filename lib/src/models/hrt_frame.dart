75983db0466db8755f73e658b2c6f9572e2f9fc8
josuemoraisgh <josuemorais@gmail.com>
2024-03-13 09:30:12 -0300

    refactor isLongFrame type

--- lib/src/models/hrt_frame.dart
+++ lib/src/models/hrt_frame.dart
@@ -8,11 +8,11 @@ import 'package:hctvirtual/src/extension/dec_bin.dart';
 import 'package:hctvirtual/src/extension/split_string.dart';
 
 class HrtFrame {
-  List<String> log = [];
+  String log = "";
   int posIniFrame = 0;
   String preamble = "";
   String delimiter = "";
-  bool isLongFrame = false;
+  bool _isLongFrame = false;
   String manufacterID = "";
   String deviceType = "";
   String address = "";
@@ -21,111 +21,117 @@ class HrtFrame {
   String _body = "";
   String checkSum = "";
 
-  HrtFrame({String? hrtFrame}) {
-    if (hrtFrame != null) {
-      extractFrame(hrtFrame);
+  HrtFrame(String? frame) {
+    if (frame != null) {
+      extractFrame(frame);
     }
   }
 
-  String calculedCheckSum() {
-    final listFrame =
-        (delimiter + address + command + nBBody.toRadixString(16) + _body)
-            .splitByLength(2);
-    return listFrame.reduce((value, element) =>
-        (int.parse(value, radix: 16) ^ int.parse(element, radix: 16))
-            .toRadixString(16));
+  String calcCheckSum() {
+    final listFrame = (delimiter +
+            manufacterID +
+            deviceType +
+            address +
+            command +
+            nBBody.toRadixString(16) +
+            _body)
+        .splitByLength(2);
+    return listFrame
+        .reduce((value, element) =>
+            (int.parse(value, radix: 16) ^ int.parse(element, radix: 16))
+                .toRadixString(16))
+        .toUpperCase()
+        .padLeft(2, '0');
   }
 
   String get frame {
-    if (isLongFrame == false) {
-      //PREAMBLE  DELIMITER  ADDRESS  COMMAND  NBBody  BODY   CheckSum
-      // xbytes    1bytes     1bytes   1bytes   1byte   xbytes 1byte
-      return preamble +
-          delimiter +
-          address +
-          command +
-          nBBody.toRadixString(16) +
-          _body +
-          calculedCheckSum();
-    } else {
-      // PREAMBLE DELIMITER MANUFACTERID DEVICETYPE ADDRESS  COMMAND  NBBody  BODY   CheckSum
-      // xbytes   1bytes    1bytes       1bytes     3bytes   1bytes   1byte   xbytes 1byte
-      return preamble +
+    String resp = "";
+    try {
+      log = "";
+      resp = preamble +
           delimiter +
           manufacterID +
           deviceType +
           address +
           command +
-          nBBody.toRadixString(16) +
+          nBBody.toRadixString(16).padLeft(2, '0') +
           _body +
-          calculedCheckSum();
+          calcCheckSum();
+    } catch (e) {
+      log = "Incorrect Value form Frame";
     }
+    return resp;
   }
 
   set frame(String hrtFrame) {
+    log = "";
     extractFrame(hrtFrame);
   }
 
   void extractFrame(String strFrame) {
-    //Calcula a posIniFrame
-    final listFrame = strFrame.splitByLength(2).join(" ");
-    int iniFrameAux =
-        listFrame.indexOf(RegExp(r'FF ([^F][A-F0-9]|[A-F0-9][^F])'));
-    if (iniFrameAux == -1) {
-      posIniFrame = 0;
-      log += ["Don't find Preamble in Frame"];
-    } else {
-      posIniFrame = (2 * iniFrameAux + 6) ~/ 3;
-    }
-    //Extrai o preamble
-    preamble = strFrame.substring(0, posIniFrame);
-    //Extrai o delimiter
-    delimiter = strFrame.substring(posIniFrame, posIniFrame + 2);
-    //Extrai o type frame
-    final value =
-        int.parse(strFrame.substring(posIniFrame, posIniFrame + 1), radix: 16);
-    isLongFrame = value.getBits(3, 1) == 1 ? true : false;
-    //Extrai o ManufacterID se houver
-    if (isLongFrame == false) {
-      manufacterID = "";
-      log += ["Short frame don't have a ManufacterID"];
-    } else {
-      manufacterID = strFrame.substring(posIniFrame + 2, posIniFrame + 4);
-    }
-    //Extrai o Device Type se houver
-    if (isLongFrame == false) {
-      deviceType = "";
-      log += ["Short frame don't have a DeviceType"];
-    } else {
-      deviceType = strFrame.substring(posIniFrame + 4, posIniFrame + 6);
-    }
-    //Extrai o Address que pode ser de um byte ou tres bytes.
-    if (isLongFrame == false) {
-      address = strFrame.substring(posIniFrame + 2, posIniFrame + 4);
-    } else {
-      address = strFrame.substring(posIniFrame + 6, posIniFrame + 12);
-    }
-    //Extrai o Command
-    if (isLongFrame == false) {
-      command = strFrame.substring(posIniFrame + 4, posIniFrame + 6);
-    } else {
-      command = strFrame.substring(posIniFrame + 12, posIniFrame + 14);
-    }
-    //Extrai o numero de bytes do body [nbbody] e o body
-    if (isLongFrame == false) {
-      nBBody = int.parse(strFrame.substring(posIniFrame + 6, posIniFrame + 8),
+    try {
+      //Calcula a posIniFrame
+      final listFrame = strFrame.splitByLength(2).join(" ");
+      int iniFrameAux =
+          listFrame.indexOf(RegExp(r'FF ([^F][A-F0-9]|[A-F0-9][^F])'));
+      if (iniFrameAux == -1) {
+        log = "Don't find Preamble in Frame";
+        return;
+      } else {
+        posIniFrame = (2 * iniFrameAux + 6) ~/ 3;
+      }
+      //Extrai o preamble
+      preamble = strFrame.substring(0, posIniFrame);
+      //Extrai o delimiter
+      delimiter = strFrame.substring(posIniFrame, posIniFrame + 2);
+      //Extrai o type frame
+      final value = int.parse(strFrame.substring(posIniFrame, posIniFrame + 1),
           radix: 16);
-      _body = strFrame.substring(posIniFrame + 8, posIniFrame + 8 + 2 * nBBody);
-    } else {
-      nBBody = int.parse(strFrame.substring(posIniFrame + 14, posIniFrame + 16),
-          radix: 16);
-      _body =
-          strFrame.substring(posIniFrame + 16, posIniFrame + 16 + 2 * nBBody);
-    }
-    //Extrai o CheckSum e checa se ele esta correto.
-    checkSum = strFrame.substring(strFrame.length - 2);
-    if (calculedCheckSum() != checkSum) {
-      log += ["Incorrect CheckSum"];
+      _isLongFrame = value.getBits(3, 1) == 1 ? true : false;
+      //Extrai o ManufacterID se houver
+      if (_isLongFrame == false) {
+        manufacterID = "";
+      } else {
+        manufacterID = strFrame.substring(posIniFrame + 2, posIniFrame + 4);
+      }
+      //Extrai o Device Type se houver
+      if (_isLongFrame == false) {
+        deviceType = "";
+      } else {
+        deviceType = strFrame.substring(posIniFrame + 4, posIniFrame + 6);
+      }
+      //Extrai o Address que pode ser de um byte ou tres bytes.
+      if (_isLongFrame == false) {
+        address = strFrame.substring(posIniFrame + 2, posIniFrame + 4);
+      } else {
+        address = strFrame.substring(posIniFrame + 6, posIniFrame + 12);
+      }
+      //Extrai o Command
+      if (_isLongFrame == false) {
+        command = strFrame.substring(posIniFrame + 4, posIniFrame + 6);
+      } else {
+        command = strFrame.substring(posIniFrame + 12, posIniFrame + 14);
+      }
+      //Extrai o numero de bytes do body [nbbody] e o body
+      if (_isLongFrame == false) {
+        nBBody = int.parse(strFrame.substring(posIniFrame + 6, posIniFrame + 8),
+            radix: 16);
+        _body =
+            strFrame.substring(posIniFrame + 8, posIniFrame + 8 + 2 * nBBody);
+      } else {
+        nBBody = int.parse(
+            strFrame.substring(posIniFrame + 14, posIniFrame + 16),
+            radix: 16);
+        _body =
+            strFrame.substring(posIniFrame + 16, posIniFrame + 16 + 2 * nBBody);
+      }
+      //Extrai o CheckSum e checa se ele esta correto.
+      checkSum = strFrame.substring(strFrame.length - 2);
+      if (calcCheckSum() != checkSum) {
+        log = "Incorrect CheckSum";
+      }
+    } catch (e) {
+      log = "Incorrect hart Frame size";
     }
   }
 
@@ -133,5 +139,14 @@ class HrtFrame {
   set body(String newBody) {
     nBBody = newBody.length ~/ 2;
     _body = newBody;
+  }
+
+  bool get isLongFrame => _isLongFrame;
+  set isLongFrame(bool newIsLongFrame) {
+    //Set frame type
+    _isLongFrame = newIsLongFrame;    
+    final value = int.parse(delimiter, radix: 16);
+    value.setBits(7, 1, _isLongFrame == true ? 1 : 0);
+    delimiter = value.toRadixString(16).padLeft(2,'0');
   }
 }
---
+++ test/src/models/hrt_frame_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hctvirtual/src/models/hrt_frame.dart';
+
+void main() {
+  //For a long Frame 3 bytes
+  test('O frame "FFF0A3EAF3DCAB970100AB" esta correto', () {
+    final frame = HrtFrame("FFF0A3EAF3DCAB970100AB");
+    expect(frame.log, "");
+    expect(frame.preamble, 'FF');
+    expect(frame.delimiter, 'F0');
+    expect(frame.isLongFrame, true);
+    expect(frame.manufacterID, 'A3');
+    expect(frame.deviceType, 'EA');
+    expect(frame.address, 'F3DCAB');
+    expect(frame.command, '97');
+    expect(frame.nBBody, 1);
+    expect(frame.body, '00');
+    expect(frame.checkSum, 'AB');
+  });
+  //For a short Frame 1 byte
+  test('O frame "FF40A3CD0100AF" esta errado CheckSum apenas', () {
+    final frame = HrtFrame("FF40A3CD0100AF");
+    expect(frame.log, "Incorrect CheckSum");
+    expect(frame.preamble, 'FF');
+    expect(frame.delimiter, '40');
+    expect(frame.isLongFrame, false);
+    expect(frame.manufacterID, '');
+    expect(frame.deviceType, '');
+    expect(frame.address, 'A3');
+    expect(frame.command, 'CD');
+    expect(frame.nBBody, 1);
+    expect(frame.body, '00');
+    expect(frame.checkSum, 'AF');
+  });
+
+  test('O frame "40A3CD0100AF" esta errado falta preamble', () {
+    final frame = HrtFrame("40A3CD0100AF");
+    expect(frame.log, "Don't find Preamble in Frame");    
+    expect(frame.preamble, '');
+    expect(frame.delimiter, '');
+    expect(frame.isLongFrame, false);
+    expect(frame.manufacterID, '');
+    expect(frame.deviceType, '');
+    expect(frame.address, '');
+    expect(frame.command, '');
+    expect(frame.nBBody, 0);
+    expect(frame.body, '');
+    expect(frame.checkSum, '');
+  });
+}
