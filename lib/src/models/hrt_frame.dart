3ef406921261eda4ddd5497c3604a65cc68ee02c
josuemoraisgh <josuemorais@gmail.com>
2024-03-13 09:37:01 -0300

    refactor delimiter

--- lib/src/models/hrt_frame.dart
+++ lib/src/models/hrt_frame.dart
@@ -11,13 +11,13 @@ class HrtFrame {
   String log = "";
   int posIniFrame = 0;
   String preamble = "";
-  String delimiter = "";
+  String _delimiter = "";
   bool _isLongFrame = false;
   String manufacterID = "";
   String deviceType = "";
   String address = "";
   String command = "";
-  int nBBody = 0;
+  int _nBBody = 0;
   String _body = "";
   String checkSum = "";
 
@@ -28,12 +28,12 @@ class HrtFrame {
   }
 
   String calcCheckSum() {
-    final listFrame = (delimiter +
+    final listFrame = (_delimiter +
             manufacterID +
             deviceType +
             address +
             command +
-            nBBody.toRadixString(16) +
+            _nBBody.toRadixString(16) +
             _body)
         .splitByLength(2);
     return listFrame
@@ -49,12 +49,12 @@ class HrtFrame {
     try {
       log = "";
       resp = preamble +
-          delimiter +
+          _delimiter +
           manufacterID +
           deviceType +
           address +
           command +
-          nBBody.toRadixString(16).padLeft(2, '0') +
+          _nBBody.toRadixString(16).padLeft(2, '0') +
           _body +
           calcCheckSum();
     } catch (e) {
@@ -83,7 +83,7 @@ class HrtFrame {
       //Extrai o preamble
       preamble = strFrame.substring(0, posIniFrame);
       //Extrai o delimiter
-      delimiter = strFrame.substring(posIniFrame, posIniFrame + 2);
+      _delimiter = strFrame.substring(posIniFrame, posIniFrame + 2);
       //Extrai o type frame
       final value = int.parse(strFrame.substring(posIniFrame, posIniFrame + 1),
           radix: 16);
@@ -114,16 +114,17 @@ class HrtFrame {
       }
       //Extrai o numero de bytes do body [nbbody] e o body
       if (_isLongFrame == false) {
-        nBBody = int.parse(strFrame.substring(posIniFrame + 6, posIniFrame + 8),
+        _nBBody = int.parse(
+            strFrame.substring(posIniFrame + 6, posIniFrame + 8),
             radix: 16);
         _body =
-            strFrame.substring(posIniFrame + 8, posIniFrame + 8 + 2 * nBBody);
+            strFrame.substring(posIniFrame + 8, posIniFrame + 8 + 2 * _nBBody);
       } else {
-        nBBody = int.parse(
+        _nBBody = int.parse(
             strFrame.substring(posIniFrame + 14, posIniFrame + 16),
             radix: 16);
-        _body =
-            strFrame.substring(posIniFrame + 16, posIniFrame + 16 + 2 * nBBody);
+        _body = strFrame.substring(
+            posIniFrame + 16, posIniFrame + 16 + 2 * _nBBody);
       }
       //Extrai o CheckSum e checa se ele esta correto.
       checkSum = strFrame.substring(strFrame.length - 2);
@@ -135,18 +136,30 @@ class HrtFrame {
     }
   }
 
+  //O nBBody só pode ser lido não escrito
+  int get nBBody => _nBBody;
+  
+  //Setando o body ja seta o nBBody
   String get body => _body;
   set body(String newBody) {
-    nBBody = newBody.length ~/ 2;
+    _nBBody = newBody.length ~/ 2;
     _body = newBody;
   }
 
+  //Alterando o isLongFrame altera o delimiter
   bool get isLongFrame => _isLongFrame;
   set isLongFrame(bool newIsLongFrame) {
-    //Set frame type
-    _isLongFrame = newIsLongFrame;    
-    final value = int.parse(delimiter, radix: 16);
+    _isLongFrame = newIsLongFrame;
+    final value = int.parse(_delimiter, radix: 16);
     value.setBits(7, 1, _isLongFrame == true ? 1 : 0);
-    delimiter = value.toRadixString(16).padLeft(2,'0');
+    _delimiter = value.toRadixString(16).padLeft(2, '0');
+  }
+
+  //Alterando o delimiter tem que alterar o isLongFrame
+  String get delimiter => _delimiter;
+  set delimiter(String newDelimiter) {
+    _delimiter = newDelimiter;
+    final value = int.parse(_delimiter, radix: 16);
+    _isLongFrame = value.getBits(7, 1) == 1 ? true : false;
   }
 }
